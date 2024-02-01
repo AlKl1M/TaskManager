18e6cb1d354de7c6bbe7e1ee95f6f5e9b8f8b34d
AlKl1M <al.klimkovetskiy@gmail.com>
2024-02-01 19:28:58 +0200

    docker compose added

--- src/main/java/com/alkl1m/taskmanager/util/JwtUtil.java
+++ src/main/java/com/alkl1m/taskmanager/util/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +17,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
-
-    public static final String SECRET = "";
+    @Value("${application.security.jwt.secret-key}")
+    public String SECRET;
 
     public String generateToken(JwtUserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
