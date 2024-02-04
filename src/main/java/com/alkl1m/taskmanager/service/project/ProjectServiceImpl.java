a3fc63a2d5b05154925929a667307fdff31002bf
AlKl1M <al.klimkovetskiy@gmail.com>
2024-02-04 13:20:28 +0200

    Deletet pagination for projects because we have limit of 20 projects per user

--- src/main/java/com/alkl1m/taskmanager/service/project/ProjectServiceImpl.java
+++ src/main/java/com/alkl1m/taskmanager/service/project/ProjectServiceImpl.java
@@ -2,9 +2,7 @@ package com.alkl1m.taskmanager.service.project;
 
 import com.alkl1m.taskmanager.dto.project.CreateProjectCommand;
 import com.alkl1m.taskmanager.dto.project.UpdateProjectCommand;
-import com.alkl1m.taskmanager.dto.project.FindProjectsQuery;
 import com.alkl1m.taskmanager.dto.project.ProjectDto;
-import com.alkl1m.taskmanager.dto.project.ProjectsPagedResult;
 import com.alkl1m.taskmanager.entity.Project;
 import com.alkl1m.taskmanager.entity.User;
 import com.alkl1m.taskmanager.enums.Status;
@@ -14,20 +12,17 @@ import com.alkl1m.taskmanager.repository.ProjectRepository;
 import com.alkl1m.taskmanager.repository.UserRepository;
 import com.alkl1m.taskmanager.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 @Transactional(readOnly = true)
 public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
@@ -35,51 +30,25 @@ public class ProjectServiceImpl implements ProjectService {
     private final HttpServletRequest httpServletRequest;
     private final JwtUtil jwtUtil;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository, HttpServletRequest httpServletRequest, JwtUtil jwtUtil) {
-        this.projectRepository = projectRepository;
-        this.userRepository = userRepository;
-        this.httpServletRequest = httpServletRequest;
-        this.jwtUtil = jwtUtil;
-    }
-
     @Override
-    public ProjectsPagedResult<ProjectDto> findProjects(FindProjectsQuery query) {
-        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
-        Long userId = jwtUtil.extractId(token);
-        Optional<User> user = userRepository.findById(userId);
-
-        if (user.isPresent()) {
-            Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
-            int pageNo = query.pageNo() > 0 ? query.pageNo() - 1 : 0;
-            Pageable pageable = PageRequest.of(pageNo, query.pageSize(), sort);
-            Page<ProjectDto> page = projectRepository.findProjects(user.get(), pageable);
-            return new ProjectsPagedResult<>(
-                    page.getContent(),
-                    page.getTotalElements(),
-                    page.getNumber() + 1,
-                    page.getTotalPages(),
-                    page.isFirst(),
-                    page.isLast(),
-                    page.hasNext(),
-                    page.hasPrevious()
-            );
-        } else {
-            return new ProjectsPagedResult<>(Collections.emptyList(), 0, 0, 0, true, true, false, false);
-        }
+    public List<ProjectDto> getAllProjects() {
+        Long userId = getCurrentUserId();
+        List<Project> projects = projectRepository.findAllByUserId(userId);
+        return projects.stream()
+                .map(ProjectDto::from)
+                .collect(Collectors.toList());
     }
 
     @Override
     @Transactional
     public ProjectDto create(CreateProjectCommand cmd) {
-        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
-        Long userId = jwtUtil.extractId(token);
+        Long userId = getCurrentUserId();
         Optional<User> user = userRepository.findById(userId);
         if (user.isPresent()) {
             int projectCount = userRepository.countUserProjects(userId);
             if (projectCount >= 20) {
                 throw new IllegalStateException("User has reached the maximum number of projects.");
             }
-
             Project project = new Project();
             project.setName(cmd.name());
             project.setDescription(cmd.description());
@@ -95,8 +64,7 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     @Transactional
     public void update(UpdateProjectCommand cmd) {
-        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
-        Long userId = jwtUtil.extractId(token);
+        Long userId = getCurrentUserId();
         Project project = projectRepository.findById(cmd.id())
                 .orElseThrow(() -> ProjectNotFoundException.of(cmd.id()));
         project.setName(cmd.name());
@@ -111,8 +79,7 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     @Transactional
     public void delete(Long id) {
-        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
-        Long userId = jwtUtil.extractId(token);
+        Long userId = getCurrentUserId();
         Project project = projectRepository.findById(id)
                 .orElseThrow(() -> ProjectNotFoundException.of(id));
         if (project.getUser().getId().equals(userId)) {
@@ -125,8 +92,7 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     @Transactional
     public void changeStatus(Long id) {
-        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
-        Long userId = jwtUtil.extractId(token);
+        Long userId = getCurrentUserId();
         Project project = projectRepository.findById(id)
                 .orElseThrow(() -> ProjectNotFoundException.of(id));
         if (project.getUser().getId().equals(userId)) {
@@ -139,9 +105,15 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public List<ProjectDto> search(String query) {
-        List<Project> projects = projectRepository.findByQuery(query);
+        Long userId = getCurrentUserId();
+        List<Project> projects = projectRepository.findByQueryAndUserId(query, userId);
         return projects.stream()
                 .map(ProjectDto::from)
                 .collect(Collectors.toList());
+    }
+
+    public Long getCurrentUserId() {
+        String token = httpServletRequest.getHeader("Authorization").replace("Bearer ", "");
+        return jwtUtil.extractId(token);
     }
 }
